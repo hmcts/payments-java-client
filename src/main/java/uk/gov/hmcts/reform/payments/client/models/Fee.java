a8dbeb0c0b66b6fe7108c5e5a0716021661f066c
Andrew Walker <andrewwa@kainos.com>
2019-08-21 10:32:56 +0100

    Add builder, noargs and allargs annotations

--- src/main/java/uk/gov/hmcts/reform/payments/client/models/Fee.java
+++ src/main/java/uk/gov/hmcts/reform/payments/client/models/Fee.java
@@ -2,12 +2,18 @@ package uk.gov.hmcts.reform.payments.client.models;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Fee {
     private BigDecimal calculatedAmount;
     private String code;
--- src/main/java/uk/gov/hmcts/reform/payments/client/models/Payment.java
+++ src/main/java/uk/gov/hmcts/reform/payments/client/models/Payment.java
@@ -3,7 +3,10 @@ package uk.gov.hmcts.reform.payments.client.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.math.BigDecimal;
@@ -12,6 +15,9 @@ import java.math.BigDecimal;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
     private String description;
     private String currency;
