da69b24aba3bcd64120dca86e8635b05d24944e4
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2021-09-08 10:45:46 +0100

    refactor:Resolve input stream closure for feign response

--- src/main/java/uk/gov/hmcts/reform/payments/client/config/FeignErrorDecoder.java
+++ src/main/java/uk/gov/hmcts/reform/payments/client/config/FeignErrorDecoder.java
@@ -3,23 +3,24 @@ package uk.gov.hmcts.reform.payments.client.config;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StreamUtils;
+import org.apache.commons.io.IOUtils;
 import uk.gov.hmcts.reform.payments.client.InvalidPaymentRequestException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-
-import static feign.FeignException.errorStatus;
 
 @Slf4j
 public class FeignErrorDecoder implements ErrorDecoder {
 
     public static final String DUPLICATE_PAYMENT = "Duplicate Payment";
 
+    private final ErrorDecoder defaultErrorDecoder = new Default();
+
     @Override
     public Exception decode(String methodKey, Response response) {
-        String responseBody = getResponseBodyString(response);
-        log.error("Response body from Payment client is {}", responseBody);
+        byte[] bytes = getBytes(response);
+        final String responseBody = new String(bytes, StandardCharsets.UTF_8);
 
         if (response.status() == 400 && responseBody.equals(DUPLICATE_PAYMENT)) {
             log.error("Error took place when using Feign client to send HTTP Request."
@@ -29,14 +30,16 @@ public class FeignErrorDecoder implements ErrorDecoder {
                     + methodKey);
             return new InvalidPaymentRequestException(responseBody);
         }
-        return errorStatus(methodKey, response);
+        return defaultErrorDecoder.decode(methodKey, response.toBuilder().body(bytes).build());
     }
 
-    private String getResponseBodyString(Response response) {
+    private byte[] getBytes(Response response) {
         try {
-            return StreamUtils.copyToString(response.body().asInputStream(), StandardCharsets.UTF_8);
+            InputStream inputStream = response.body().asInputStream();
+            return IOUtils.toByteArray(inputStream);
         } catch (IOException e) {
-            return "error read response body. " + e.getMessage();
+            log.error("Failed to read the response body with error: ", e);
         }
+        return null;
     }
 }
