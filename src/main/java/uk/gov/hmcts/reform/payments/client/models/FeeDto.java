8406314ab832cde46fd5d3cf31c3f4bd66d6e071
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2022-04-04 13:22:08 +0100

    Update FeeDto.java

--- src/main/java/uk/gov/hmcts/reform/payments/client/models/FeeDto.java
+++ src/main/java/uk/gov/hmcts/reform/payments/client/models/FeeDto.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.payments.client.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
